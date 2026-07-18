088afa5e6fe1d6e7e2fc41efa42a7b03e0869d4c
Alec Ours <a.ours1999@gmail.com>
2026-07-18 09:58:48 -0700

    Add functionality to show new to-do on screen

--- script.js
+++ script.js
@@ -8,16 +8,26 @@ class toDoItem {
     }
 }
 
-function CreateToDoItem() {
+function createToDoItem() {
     let addToDoBtn = document.querySelector(".new-to-do");
     let toDoForm = document.querySelector("#to-do-form");
+
     addToDoBtn.addEventListener("click", (e) => {
         toDoForm.style.display = "block";
     });
-    let newItem = new toDoItem("clean", "Finish the dishes, and the laundry", "Monday", "High", "Checklist goes here")
-    console.log(newItem);
-    addToDoItem(newItem);
-};
+
+    toDoForm.addEventListener("submit", (e) => {
+        event.preventDefault();
+
+        const formData = new FormData(toDoForm);
+        const data = Object.fromEntries(formData);
+
+        resetToDoForm(toDoForm);
+        testAddToDoItem(data);
+        
+        console.log(data);
+    });
+}
 
 function addToDoItem(toDoItem) {
     const mainContent = document.querySelector(".main-content");
@@ -27,4 +37,26 @@ function addToDoItem(toDoItem) {
     mainContent.appendChild(toDoArea);
 }
 
-CreateToDoItem();
+function resetToDoForm(toDoForm) {
+    toDoForm.style.display = "none";
+}
+
+createToDoItem();
+
+function testAddToDoItem(formData) {
+    const mainContent = document.querySelector(".main-content");
+    let toDoArea = document.createElement("div");
+    
+    let newToDo = new toDoItem("clean", "Finish the dishes", "Monday", "High", "Checklist goes here");
+
+    let itemTitle = newToDo.title;
+    let itemDescription = newToDo.description;
+    let itemDueDate = newToDo.dueDate;
+    let itemPriority = newToDo.priority;
+    let itemCheckList = newToDo.checklist;
+
+    toDoArea.append(itemTitle, ", ", itemDescription, ", ", itemDueDate, ", ", itemPriority, ", ", itemCheckList);
+    toDoArea.classList.add("to-do-item");
+    mainContent.appendChild(toDoArea);
+
+}
